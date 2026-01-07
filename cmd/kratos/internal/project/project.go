7fefee7bce562e74ba28c3f49a4608f19b3bff67
xiaogang.du <xiaogang.du@funplus.com>
2026-01-07 17:18:12 +0800

    change layout source

--- cmd/kratos/internal/project/project.go
+++ cmd/kratos/internal/project/project.go
@@ -32,7 +32,8 @@ var (
 
 func init() {
 	if repoURL = os.Getenv("KRATOS_LAYOUT_REPO"); repoURL == "" {
-		repoURL = "https://github.com/go-kratos/kratos-layout.git"
+		//repoURL = "https://github.com/go-kratos/kratos-layout.git"
+		repoURL = "https://github.com/duxiaogang/kratos-layout.git"
 	}
 	timeout = "60s"
 	CmdNew.Flags().StringVarP(&repoURL, "repo-url", "r", repoURL, "layout repo")
